5736a02d114d6636824029fdb8da283d05a8577b
Kneelawk <jedidiah.pommert@gmail.com>
2020-04-04 13:59:43 -0700

    Initial project setup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Kneelawk on 4/4/20.
+ */
+module com.kneelawk.modpackattributor {
+    exports com.kneelawk.modpackattributor;
+
+    requires kotlin.stdlib;
+}
---
+++ src/main/kotlin/com/kneelawk/modpackattributor/ModpackAttributorApp.kt
@@ -0,0 +1,5 @@
+package com.kneelawk.modpackattributor
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
